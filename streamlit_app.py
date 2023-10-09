63e3c55bfdb8722ce286d0186fb7e7259871f6e1
tunneyet <147450259+tunneyet@users.noreply.github.com>
2023-10-09 22:17:10 +0000

    TEST

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.write("This is a sample demo. If you're following along during the workshop, 
 
 st.write("For more details, take a look at this workshop's [walkthrough](https://msu-ai.notion.site/LangChain-Apps-MSU-AI-Club-7fbdc17aa0f6415a8d7606cfb1e41e00)")
 
-
+str.write("HAHAHAHAHA")
 # Uncomment the line below to add a text field, and type "streamlit run streamlit_app.py" in the terminal to see the changes.
 
-# st.text_input("Type something here!")
+str.text_input("HAHAHAHAAH")
